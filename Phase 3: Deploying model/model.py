7312b29c204911aee01a52c7562c86c523815a00
iam-Dylan <phamduy1032003@gmail.com>
2024-01-04 03:21:11 +0700

    update model in phase 3

--- Phase 3: Deploying model/model.py
+++ Phase 3: Deploying model/model.py
@@ -2,40 +2,30 @@ import pandas as pd
 import pickle
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics import mean_squared_error
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+from sklearn.linear_model import SGDRegressor
 
-import warnings
-warnings.filterwarnings('ignore')
-
-data = pd.read_csv('processed_data.csv')
-
-# define target feature
-target_feature = ['rating']
+data = pd.read_csv('Data/processed_data.csv')
 
 # define numerical and categorical features
 numerical_features = ['enrollment', 'duration', 'instructor_rate']
-categorical_features = list(data.select_dtypes(include="object").columns)
-categorical_features.remove('name')
-categorical_features.remove('instructor')
+categorical_features = ['general', 'specify', 'language', 'level', 'offered by']
 
 # select specific features
-selected_features = numerical_features + categorical_features
+selected_features = ['enrollment', 'duration', 'instructor_rate', 'general', 'specify', 'language', 'level', 'offered by']
 # handle missing values
-data.dropna(subset=numerical_features+target_feature, inplace=True)
+data.dropna(inplace=True)
 
 # select features and target variable
 X = data[selected_features]
-y = data[target_feature]
+y = data['rating']
 
-# split the data into training, testing, and validation sets
-X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=2/3, random_state=42)
+# split the data into training, testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
 # create transformers for numerical and categorical features
 numerical_transformer = Pipeline(steps=[
@@ -52,33 +42,18 @@ preprocessor = ColumnTransformer(
         ('num', numerical_transformer, numerical_features),
         ('cat', categorical_transformer, categorical_features)
     ])
-# fit the ColumnTransformer on the training data
-preprocessor.fit(X_train)
 
-# combine preprocessing with model training in a pipeline
-modelNN = Pipeline(steps=[
+# defime best param we got above
+param = {'alpha': 0.001,
+ 'learning_rate': 'adaptive',
+ 'max_iter': 500}
+
+model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('regressor', Sequential([
-        Dense(64, activation='relu', input_dim=preprocessor.transform(X_train).shape[1]),
-        Dense(32, activation='relu'),
-        Dense(1, activation='linear')
-    ]))
+    ('regressor', SGDRegressor(**param))
 ])
 
-# training
-best_lr = 0.02
-best_bz = 200
-
-optimizer = Adam(learning_rate=best_lr)
-modelNN.named_steps['regressor'].compile(optimizer=optimizer, loss='mean_squared_error')
-
-modelNN.named_steps['regressor'].fit(
-        preprocessor.transform(pd.concat([X_train,X_val])), pd.concat([y_train,y_val]),
-        epochs=50, batch_size=best_bz,verbose=0)
-
-# testing
-y_test_pred = modelNN.predict(X_test)
-nn_mse = mean_squared_error(y_test, y_test_pred)
+model.fit(X_train, y_train)
 
 # saving model
-pickle.dump(modelNN, open('model.pkl','wb'))
+pickle.dump(model, open('Phase 3: Deploying model/model.pkl','wb'))
